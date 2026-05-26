dd10195733ecb6d114cbd60938f987ba473e9afc
caijiali <caijiali004@gmail.com>
2026-05-26 23:01:40 +0800

    test2

--- static/js/particles-config.js
+++ static/js/particles-config.js
@@ -1,67 +1,73 @@
-particlesJS("particles-js", {
-  particles: {
-    // 粒子数量
-    number: {
-      value: 200,
-      density: {
-        enable: true,
-        value_area: 1000
-      }
-    },
-    color: {
-      value: "#000000"
-    },
-    shape: {
-      type: "circle"
-    },
-    opacity: {
-      value: 0.7,
-    },
-    // 大小
-    size: {
-      value: 2,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 1,
-        size_min: 0.3,
-        sync: false
-      }
-    },
-	line_linked: {
-      enable: false
-    },
-    // 移动
-    move: {
-      enable: true,
+document.addEventListener("DOMContentLoaded", function () {
 
-      // 向上漂浮
-      direction: "top",
+    console.log("particles start");
 
-      // 随机漂动
-      random: true,
+    particlesJS("particles-js", {
+	  particles: {
+		// 粒子数量
+		number: {
+		  value: 200,
+		  density: {
+			enable: true,
+			value_area: 1000
+		  }
+		},
+		color: {
+		  value: "#000000"
+		},
+		shape: {
+		  type: "circle"
+		},
+		opacity: {
+		  value: 0.7,
+		},
+		// 大小
+		size: {
+		  value: 2,
+		  random: true,
+		  anim: {
+			enable: true,
+			speed: 1,
+			size_min: 0.3,
+			sync: false
+		  }
+		},
+		line_linked: {
+		  enable: false
+		},
+		// 移动
+		move: {
+		  enable: true,
 
-      // 不直线
-      straight: false,
+		  // 向上漂浮
+		  direction: "top",
 
-      // 慢速
-      speed: 0.8,
+		  // 随机漂动
+		  random: true,
 
-      // 出界继续生成
-      out_mode: "out",
+		  // 不直线
+		  straight: false,
 
-      bounce: false
-    }
-  
-  },
+		  // 慢速
+		  speed: 0.8,
 
-  interactivity: {
-    events: {
-      onhover: {
-        enable: false
-      }
-    }
-  },
+		  // 出界继续生成
+		  out_mode: "out",
 
-  retina_detect: true
+		  bounce: false
+		}
+	  
+	  },
+
+	  interactivity: {
+		events: {
+		  onhover: {
+			enable: false
+		  }
+		}
+	  },
+
+	  retina_detect: true
+	});
+
 });
